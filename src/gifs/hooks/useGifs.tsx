8cd09dd2c75b8950d56389f94977225160d23686
unknown <MarioAG1>
2025-11-21 12:27:26 +0100

    Modificacion para utilizar useRef, para mantenerlo dentro del componente

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 
-const gifsCache: Record<string, Gif[]> = {};
+// const gifsCache: Record<string, Gif[]> = {};
 
 const useGifs = () => {
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
   const [gifs, setgifs] = useState<Gif[]>([]);
 
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
   const handleTermClicked = async (term: string) => {
-    if (gifsCache[term]) {
-      setgifs(gifsCache[term]);
+    if (gifsCache.current[term]) {
+      setgifs(gifsCache.current[term]);
       return;
     }
     const gifs = await getGifsByQuery(term);
@@ -45,7 +47,7 @@ const useGifs = () => {
 
     setgifs(gifs);
 
-    gifsCache[query] = gifs;
+    gifsCache.current[query] = gifs;
 
     console.log(gifsCache);
   };
